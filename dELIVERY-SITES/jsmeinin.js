8800d9371b27adc245ec63c5413e7a2f78211b7f
Deri Dev <aderitventur@gmail.com>
2026-08-10 17:00:14 +0100

    git

---
+++ dELIVERY-SITES/jsmeinin.js
@@ -0,0 +1,50 @@
+/*for (var i = 0; i <= 3; i++) {
+    console.log("This is line ", i)
+}
+
+*/
+/*for ( let a = 5; a >= 1; a--){
+    console.log(a);
+}
+ console.log('Counting completed!');
+ */
+
+/* let i = 1;
+ while(i <= 5){
+    console.log(i);
+    i++;
+ }
+ console.log('Countdown finished!');
+
+let i = 5
+while(i >= 1){
+    console.log(i)
+    i--;
+}
+console.log('Countdown finished!');*/
+
+/*let year = 2017;
+while(year < 2022){
+    year++;
+    console.log(year)
+}*/
+
+/*for(var year = 2023; year < 2025; year++){
+    console.log(year)
+}*/
+
+// Agora vamos fazer aninhado
+
+
+/*for(var year = 2023; year < 2025; year++){
+    console.log(year)
+    for(var month = 6; month < 9 ; month++)
+        console.log("------" ,month)
+}
+let dery ='ADERITOVENTURA';
+
+for(let a = 0; a < dery.length; a++){
+    console.log("Su nombre--", dery[a])
+}*/
+
+for(let i = 1; )
